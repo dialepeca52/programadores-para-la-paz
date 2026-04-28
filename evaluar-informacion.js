3572017d00c73ddd3d5cdb0fd8a9c55def89f6bb
mmgranda <mariadelmgranda15@gmail.com>
2026-04-28 14:31:56 -0500

    mi  primer commit, subir repositorio

---
+++ evaluar-informacion.js
@@ -0,0 +1,13 @@
+function evaluarInformacion(tieneFuente, estaVerificada) {
+    if (tieneFuente === true && estaVerificada === true) {
+      return "La información puede compartirse con responsabilidad."
+    } else {
+      return "La información debe revisarse antes de compartirse."
+    }
+  }
+  
+  const resultado = evaluarInformacion(true, false)
+  
+  console.log("Evaluación de información:")
+  console.log(resultado)
+  
---
+++ funcion-saludo.js
@@ -0,0 +1,7 @@
+function saludarComunidad(nombreComunidad) {
+    console.log("Bienvenida comunidad:", nombreComunidad)
+    console.log("Hoy seguimos aprendiendo JavaScript paso a paso.")
+  }
+  
+  saludarComunidad("Programadores para la Paz")
+  
