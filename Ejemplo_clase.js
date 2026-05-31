8b983fa993ff3b225ebcb8d45d375981b9400c4b
Diego Alejandro Peña Calderón <dialepeca52@gmail.com>
2026-05-31 00:32:11 -0500

    Algoritmos simples con  java script

---
+++ Ejemplo_clase.js
@@ -0,0 +1,10 @@
+var pensamientoCritico = "Una variable en programación es una caja que almacena alguna instrucción";
+console.log(pensamientoCritico);
+
+const cajita = 15;
+console.log(cajita);
+cajita = 2000;  
+console.log(cajita);
+
+let color = "Rojo";
+console.log(color);
--- decision-comunitaria.js
+++ decision-comunitaria.js
@@ -1,12 +1,17 @@
 const propuesta = "Mejorar la comunicación del barrio"
 let tieneLenguajeClaro = true
 let pideDatosNecesarios = true
+let tienebuenaintencion = true
 
 console.log("Revisión de propuesta comunitaria")
 console.log("Propuesta:", propuesta)
 
-if (tieneLenguajeClaro === true && pideDatosNecesarios === true) {
+if (tieneLenguajeClaro === true && pideDatosNecesarios === true &&
+  tienebuenaintencion === false) {
   console.log("La propuesta está lista para ser compartida con la comunidad.")
+} if (tieneLenguajeClaro === true && pideDatosNecesarios === true &&
+  tienebuenaintencion === true) {
+  console.log("La propuesta está excesivamente bien.")
 } else {
-  console.log("La propuesta debe revisarse antes de compartirse.")
+  console.log("La propuesta no está lista para ser compartida con la comunidad.")
 }
